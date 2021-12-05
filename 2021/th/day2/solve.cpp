717fe809822576de3782f7cf9bfbd9b656feca59
Tom Hall <tomhall95@outlook.com>
2021-12-05 16:22:27 +0000

    Add day 3

--- 2021/th/day2/solve.cpp
+++ 2021/th/day2/solve.cpp
@@ -9,10 +9,51 @@ int main() {
 
     std::vector<std::pair<std::string, int>> pairs;
 
+    // Split lines into pairs of strings and ints
     for (const auto& line : input) {
-        
+        pairs.push_back(std::make_pair(
+            line.substr(0, line.find(" ")),
+            std::stoi(line.substr(line.find(" ")+1, line.length()))
+        ));
     }
 
-    printf("There are %u measurements greater than the previous measurement.\n", result);
-    printf("There are %u 3-sums larger than the previous one.\n", result2);
+    // Solve part 1
+    int h_pos = 0;
+    int depth = 0;
+
+    for (const auto& pair : pairs) {
+        if (pair.first == "forward") {
+            h_pos += pair.second;
+        } else {
+            if (pair.first == "up")
+                depth -= pair.second;
+            if (pair.first == "down")
+                depth += pair.second;
+        }
+    }
+
+    std::cout << "Horizonal 1: " << h_pos << std::endl;
+    std::cout << "Depth 1: " << depth << std::endl;
+    std::cout << "Answer 1: " << h_pos * depth << std::endl;
+
+    // Solve part 2
+    h_pos = 0;
+    depth = 0;
+    int aim = 0;
+
+    for (const auto& pair : pairs) {
+        if (pair.first == "forward") {
+            h_pos += pair.second;
+            depth += aim * pair.second;
+        } else {
+            if (pair.first == "up")
+                aim -= pair.second;
+            if (pair.first == "down")
+                aim += pair.second;
+        }
+    }
+
+    std::cout << "Horizontal 2: " << h_pos << std::endl;
+    std::cout << "Depth 2: " << depth << std::endl;
+    std::cout << "Answer 2: " << h_pos * depth << std::endl;
 }
---
+++ 2021/th/day3/solve.cpp
@@ -0,0 +1,81 @@
+// TH 2021
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <vector>
+
+#include "include/utils.h"
+
+int MostCommonBit(const std::list<int>& nums, int bit_idx) {
+    // Where bit_idx 0 = LSB
+    int sum = 0;
+    for (const auto& num : nums) {
+        sum += (num & (1 << bit_idx)) >> bit_idx;
+    }
+
+    return static_cast<size_t>(sum) >= (nums.size() + 1) / 2;
+}
+
+int main() {
+    std::vector<std::string> input = ReadFileLines("input.txt");
+
+    std::list<int> ints;
+
+    for (const auto& line : input) {
+        // Convert binary string to integer
+        ints.push_back(std::stoi(line, nullptr, 2));
+    }
+
+    int length = input[0].length();
+
+    // Part 1
+
+    int gamma = 0;
+    int epsilon = 0;
+    for (int i=0; i<length; ++i) {
+        if (MostCommonBit(ints, i)) {
+            gamma |= 1 << i;
+        } else {
+            epsilon |= 1 << i;
+        }
+    }
+
+    std::cout << "===== Part 1 =====" << std::endl;
+    std::cout << "Gamma: " << gamma << std::endl;
+    std::cout << "Epsilon: " << epsilon << std::endl;
+    std::cout << "Answer: " << gamma * epsilon << std::endl;
+
+    // Part 2
+    
+    // Oxygen
+    std::list<int> oxy = ints;
+    for (int i = length-1; oxy.size() > 1 && i >= 0; --i) {
+        int mcb = MostCommonBit(oxy, i);
+        for (auto it = oxy.begin(); oxy.size() > 1 && it != oxy.end(); ) {
+            if ( ((*it & (1 << i)) >> i) != mcb ) {
+                it = oxy.erase(it);
+            } else{
+                ++it;
+            }
+        }
+    } 
+
+    // CO2
+    std::list<int> co2 = ints;
+    for (int i = length-1; co2.size() > 1 && i >= 0; --i) {
+        int mcb = MostCommonBit(co2, i);
+        for (auto it = co2.begin(); co2.size() > 1 && it != co2.end(); ) {
+            if ( ((*it & (1 << i)) >> i) == mcb ) {
+                it = co2.erase(it);
+            } else{
+                ++it;
+            }
+        }
+    }  
+
+    std::cout << "===== Part 2 =====" << std::endl;
+    std::cout << "Oxygen: " << oxy.front() << std::endl;
+    std::cout << "CO2: " << co2.front() << std::endl;
+    std::cout << "Answer: " << oxy.front() * co2.front() << std::endl;
+}
